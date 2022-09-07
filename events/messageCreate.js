865ff84371a0106ace4b651e5fccfefbc0898b08
thegears <semh.1362@gmail.com>
2022-09-07 12:24:12 +0300

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,5 +1,4 @@
 module.exports = {
-    name: 'messageCreate',
     async run(client, message) {
         client.interactions.filter((int) => int.type == 'message').find((int) => message.content.startsWith(int.name))?.run({
             client,
